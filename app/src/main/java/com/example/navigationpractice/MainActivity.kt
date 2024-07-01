192f8597147ce7cedf8374cc72be04a3c6cc2b55
sheldondcosta <ssheldondcosta@gmail.com>
2024-07-01 15:40:10 -0400

    Setting up nested navigation

--- app/src/main/java/com/example/navigationpractice/MainActivity.kt
+++ app/src/main/java/com/example/navigationpractice/MainActivity.kt
@@ -9,8 +9,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationpractice.ui.SampleViewModel
 import com.example.navigationpractice.ui.theme.NavigationPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,16 +29,65 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavigationPracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+
             }
         }
     }
 }
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("about"){
+
+        }
+        navigation(
+            startDestination = "login",
+            route = "auth",
+        ) {
+            composable("login") {
+                val viewModel = it.sharedViewModel<SampleViewModel>(navController)
+
+            }
+            composable("register") {
+                val viewModel = it.sharedViewModel<SampleViewModel>(navController)
+
+            }
+            composable("forgot_password") {
+                val viewModel = it.sharedViewModel<SampleViewModel>(navController)
+
+            }
+
+
+        }
+        navigation(
+            startDestination = "calender_overview",
+            route = "calendar"
+        ){
+            composable("calendar_overview"){
+
+            }
+            composable("calendar_entry"){
+
+            }
+
+        }
+
+
+    }
+}
+@Composable
+inline fun <reified T: ViewModel> NavBackStackEntry.sharedViewModel(navController: NavController): T{
+    val navGraphRoute = destination.parent?.route ?: return viewModel()
+    val parentEntry = remember(this){
+        navController.getBackStackEntry(navGraphRoute)
+    }
+    return viewModel(parentEntry)
+
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
---
+++ app/src/main/java/com/example/navigationpractice/ui/SampleViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.navigationpractice.ui
+
+import androidx.lifecycle.ViewModel
+
+class SampleViewModel: ViewModel() {
+}
